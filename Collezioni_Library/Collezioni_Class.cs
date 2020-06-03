2b97398dc47454bff2a914bbb5b051e75e3384e9
AlexBuratta13 <alex,buratta13@gmail.com>
2020-06-03 10:45:17 +0200

    Add project files.

---
+++ Collezioni_Library/Collezioni_Class.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collezioni_Library
+{
+    public class Collezioni_Class
+    {
+        public static bool StringheUguali(string a, string b)
+        {
+            if(a == b)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool SpaziStringa(string frase)
+        {
+            if (frase.Contains(" "))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Console_Collezioni/Console_Collezioni.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Collezioni_Library;
+
+namespace Console_Collezioni
+{
+    class Console_Collezioni
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("inserisci prima frase");
+            string a = Console.ReadLine();
+            Console.WriteLine("Inserisci seconda frase");
+            string b = Console.ReadLine();
+            bool stringheUguali = Collezioni_Class.StringheUguali(a, b);
+            Console.WriteLine("Le frasi sono uguali? " +stringheUguali);
+
+            Console.WriteLine("inserisci una frase");
+            string frase = Console.ReadLine();
+            bool spazi = Collezioni_Class.SpaziStringa(frase);
+            Console.WriteLine("La frase contiene spazi? " + spazi);
+            Console.ReadLine();
+        }
+    }
+}
